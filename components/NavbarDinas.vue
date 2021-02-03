cb430c79fce89bdeab24b34b31ccccdc786d20fb
fathiyatia <fathiya_12@apps.ipb.ac.id>
2021-02-03 16:51:47 +0700

    navbar component

---
+++ components/NavbarDinas.vue
@@ -0,0 +1,203 @@
+<template>
+  <v-navigation-drawer color="accent" v-model="drawer" fixed app width="260"
+    ><v-container>
+      <v-row no-gutters class="pt-4">
+        <v-col cols="4">
+          <v-avatar color="secondary" size="36"> </v-avatar>
+        </v-col>
+        <v-col cols="8" class="white--text">
+          <h3 class="pt-1">SITPI</h3>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-list>
+      <!--- Dashboard --->
+      <v-list-item-group color="white">
+        <v-list-item
+          v-for="(item, i) in dashboard"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text" v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!--- Tangkapan --->
+      <v-divider class="mx-3 mb-2" color="white"></v-divider>
+      <span class="ml-3 caption white--text">Tangkapan Ikan</span>
+      <v-list-item-group color="white">
+        <v-list-item
+          v-for="(item, i) in caught"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content class="white--text">
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!--- Lelang --->
+      <v-divider class="mx-3 mb-2" color="white"></v-divider>
+      <span class="ml-3 caption white--text">Lelang</span>
+      <v-list-item-group color="white">
+        <v-list-item
+          v-for="(item, i) in auction"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text" v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!--- Transaksi --->
+      <v-divider class="mx-3 mb-2" color="white"></v-divider>
+      <span class="ml-3 caption white--text">Transaksi</span>
+      <v-list-item-group color="white">
+        <v-list-item
+          v-for="(item, i) in transaction"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content class="white--text">
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!--- Pendataan --->
+      <v-divider class="mx-3 mb-2" color="white"></v-divider>
+      <span class="ml-3 caption white--text">Pendataan</span>
+      <v-list-item-group color="white">
+        <v-list-item
+          v-for="(item, i) in data"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content class="white--text">
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!--- Laporan ---->
+      <v-divider class="mx-3 mb-2" color="white"></v-divider>
+      <v-list-item
+        v-for="(item, i) in report"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon color="white">{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title class="white--text" v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: {
+    value: Boolean
+  },
+  data() {
+    return {
+      drawer: this.$props.value,
+      dashboard: [
+        {
+          icon: "mdi-view-dashboard",
+          title: "Dashboard",
+          to: "/"
+        }
+      ],
+      caught: [
+        {
+          icon: "mdi-fish",
+          title: "Data Tangkapan Ikan",
+          to: "/tangkapan/dataTangkapan"
+        }
+      ],
+      auction: [
+        {
+          icon: "mdi-handshake",
+          title: "Data Proses Lelang",
+          to: "/pelelangan/dataPelelangan"
+        }
+      ],
+      transaction: [
+        {
+          icon: "mdi-cash-multiple",
+          title: "Data Transaksi",
+          to: "/transaksi/dataTransaksi"
+        }
+      ],
+      data: [
+        {
+          icon: "mdi-sail-boat",
+          title: "Nelayan",
+          to: "/nelayan/dataNelayan"
+        },
+        {
+          icon: "mdi-account",
+          title: "Pembeli",
+          to: "/pembeli/dataPembeli"
+        },
+        {
+          icon: "mdi-map-marker",
+          title: "Daerah Tangkapan",
+          to: "/daerah/dataDaerah"
+        },
+        {
+          icon: "mdi-hook",
+          title: "Alat Tangkap",
+          to: "/alat/dataAlat"
+        }
+      ],
+      report: [
+        {
+          icon: "mdi-file-chart",
+          title: "Laporan",
+          to: "/"
+        }
+      ]
+    };
+  },
+  watch: {
+    value(value) {
+      this.drawer = value;
+    },
+    drawer(value) {
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,137 +1,78 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      color="primary"
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-      width="300"
-      ><v-container>
-        <v-row no-gutters class="pt-4">
-          <v-col cols="4">
-            <v-avatar color="secondary" size="36"> </v-avatar>
-          </v-col>
-          <v-col cols="8" class="white--text">
-            <h3 class="pt-1">SITPI</h3>
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <v-list>
-        <!--- Dashboard --->
-        <v-list-item-group color="white">
-          <v-list-item
-            v-for="(item, i) in dashboard"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-            <v-list-item-action>
-              <v-icon color="white">{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title class="white--text" v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-        <!--- Tangkapan --->
-        <v-divider class="mx-3 mb-2" color="white"></v-divider>
-        <span class="ml-3 caption white--text">Tangkapan Ikan</span>
-        <v-list-item-group color="white">
-          <v-list-item
-            v-for="(item, i) in caught"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-            <v-list-item-action>
-              <v-icon color="white">{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content class="white--text">
-              <v-list-item-title v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-        <!--- Lelang --->
-        <v-divider class="mx-3 mb-2" color="white"></v-divider>
-        <span class="ml-3 caption white--text">Lelang</span>
-        <v-list-item
-          v-for="(item, i) in auction"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon color="white">{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title class="white--text" v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-        <!--- Transaksi --->
-        <v-divider class="mx-3 mb-2" color="white"></v-divider>
-        <span class="ml-3 caption white--text">Transaksi</span>
-        <v-list-item-group color="white">
-          <v-list-item
-            v-for="(item, i) in transaction"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-            <v-list-item-action>
-              <v-icon color="white">{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content class="white--text">
-              <v-list-item-title v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-        <!--- Pendataan --->
-        <v-divider class="mx-3 mb-2" color="white"></v-divider>
-        <span class="ml-3 caption white--text">Pendataan</span>
-        <v-list-item-group color="white">
-          <v-list-item
-            v-for="(item, i) in data"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-            <v-list-item-action>
-              <v-icon color="white">{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content class="white--text">
-              <v-list-item-title v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-        <!--- Laporan ---->
-        <v-divider class="mx-3 mb-2" color="white"></v-divider>
-        <v-list-item
-          v-for="(item, i) in report"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon color="white">{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title class="white--text" v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <navbar-office v-model="drawer" v-if="office"></navbar-office>
+    <navbar-dinas v-model="drawer" v-else></navbar-dinas>
+    <!-- Top Navbar --->
     <v-app-bar color="primary" :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon color="white" @click.stop="drawer = !drawer" />
       <v-toolbar-title class="white--text" v-text="title" />
+      <v-spacer></v-spacer>
+      <!-- Toggle Button for User --->
+      <template>
+        <div class="text-center">
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="true"
+            :nudge-width="50"
+            offset-y
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                class="text-capitalize"
+                color="white"
+                dark
+                text
+                tile
+                v-bind="attrs"
+                v-on="on"
+              >
+                Rahmat | TPI Indramayu <v-icon>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="font-weight-bold accent--text"
+                      >Rahmat</v-list-item-title
+                    >
+                    <v-list-item-subtitle class="pt-1 accent--text"
+                      >Username : rahmat123</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-title
+                    ><v-btn
+                      color="primary"
+                      text
+                      class="text-capitalize"
+                      :to="'/user/ubahPassword'"
+                    >
+                      Ubah Kata Sandi
+                    </v-btn></v-list-item-title
+                  >
+                </v-list-item>
+              </v-list>
+              <v-divider></v-divider>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-title
+                    ><v-btn color="primary" block primary :to="'/'">
+                      Keluar
+                    </v-btn></v-list-item-title
+                  >
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-menu>
+        </div>
+      </template>
     </v-app-bar>
 
     <v-main>
@@ -148,80 +89,16 @@
 export default {
   data() {
     return {
+      office: true,
       clipped: false,
       drawer: false,
       fixed: false,
-      dashboard: [
-        {
-          icon: "mdi-view-dashboard",
-          title: "Dashboard",
-          to: "/"
-        }
-      ],
-      caught: [
-        {
-          icon: "mdi-clipboard-edit",
-          title: "Isi Tangkapan Ikan",
-          to: "/tangkapan/formTangkapan"
-        },
-        {
-          icon: "mdi-fish",
-          title: "Data Tangkapan Ikan",
-          to: "/tangkapan/dataTangkapan"
-        }
-      ],
-      auction: [
-        {
-          icon: "mdi-handshake",
-          title: "Data Proses Lelang",
-          to: "/pelelangan/dataPelelangan"
-        }
-      ],
-      transaction: [
-        {
-          icon: "mdi-clipboard-edit",
-          title: "Isi Transaksi",
-          to: "/transaksi/formTransaksi"
-        },
-        {
-          icon: "mdi-cash-multiple",
-          title: "Data Transaksi",
-          to: "/transaksi/dataTransaksi"
-        }
-      ],
-      data: [
-        {
-          icon: "mdi-sail-boat",
-          title: "Nelayan",
-          to: "/nelayan/dataNelayan"
-        },
-        {
-          icon: "mdi-account",
-          title: "Pembeli",
-          to: "/pembeli/dataPembeli"
-        },
-        {
-          icon: "mdi-map-marker",
-          title: "Daerah Tangkapan",
-          to: "/daerah/dataDaerah"
-        },
-        {
-          icon: "mdi-hook",
-          title: "Alat Tangkap",
-          to: "/alat/dataAlat"
-        }
-      ],
-      report: [
-        {
-          icon: "mdi-file-chart",
-          title: "Laporan",
-          to: "/"
-        }
-      ],
+      menu: false,
+
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: "Sistem Informasi TPI"
+      title: ""
     };
   }
 };
