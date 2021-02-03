58033fabda4eccfc12fc0612570899220bc74061
fathiyatia <fathiya_12@apps.ipb.ac.id>
2021-02-03 16:51:10 +0700

    login page

---
+++ layouts/blank.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container
+        fluid
+        class="d-flex justify-center align-center full-height ma-0 pa-8 background_login"
+      >
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  computed: {
+    is_dark() {
+      return this.$vuetify.theme.isDark;
+    }
+  }
+};
+</script>
+
+<style>
+.full-height {
+  height: 100%;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card max-width="1200px" class="pa-7 rounded-lg" tile elevation="5">
+    <v-card-title class="d-flex justify-center">
+      Aplikasi Pelelangan Ikan Terintegrasi
+    </v-card-title>
+    <v-card-subtitle class="d-flex justify-center">
+      Isi username dan password untuk masuk ke dalam sistem
+    </v-card-subtitle>
+    <v-form class="pt-3" ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        outlined
+        single-line
+        :rules="required"
+        label="Username"
+        prepend-inner-icon="mdi-account-circle"
+      />
+      <v-text-field
+        outlined
+        single-line
+        :rules="required"
+        label="Password"
+        prepend-inner-icon="mdi-lock"
+        :type="showPassword ? 'text' : 'password'"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="handleclick"
+      />
+      <span>Masuk sebagai : </span>
+      <v-radio-group row class="check" :rules="required">
+        <v-radio class="pb-3" label="Petugas TPI" value="1"></v-radio>
+        <v-radio class="pb-3" label="Dinas Kabupaten" value="2"></v-radio>
+      </v-radio-group>
+    </v-form>
+    <v-card-actions class="px-0">
+      <v-btn color="primary" block @click="login()">Masuk</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  layout: "blank",
+  data() {
+    return {
+      showPassword: false,
+      valid: true,
+      required: [v => !!v || "Data ini harus diisi"]
+    };
+  },
+  methods: {
+    handleclick() {
+      this.showPassword = !this.showPassword;
+    },
+    login() {
+      if (this.$refs.form.validate()) {
+        this.$router.push("/");
+      }
+    }
+  }
+};
+</script>
