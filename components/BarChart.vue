71fbe9516bc504d0f407989c66574357cbc97443
fathiyatia <fathiya_12@apps.ipb.ac.id>
2021-03-11 21:38:54 +0700

    add chart component

---
+++ components/BarChart.vue
@@ -0,0 +1,26 @@
+<script>
+//import { HorizontalBar } from "vue-chartjs";
+import { Bar } from "vue-chartjs";
+export default {
+  //extends: HorizontalBar,
+  extends: Bar,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+  methods: {
+    update() {
+      this.$data._chart.update();
+    }
+  }
+};
+</script>
